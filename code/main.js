7aa4695c9737efd6d2e7a5cc3a87dded3505e14d
AI-DataWizard <tadiwachops3567@gmail.com>
2026-01-21 00:17:47 +0200

    first commit

---
+++ code/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  /*
+  $("#search, #icon").mouseenter(function () {
+    $(".logo").hide();
+  });
+
+  $("#search, #icon").mouseleave(function () {
+    $(".logo").show();
+  });
+  */
+
+  $(".fa-bars").click(function () {
+    $(".navbar").toggle();
+  });
+
+  function hideElementsOnWidth() {
+    if ($(window).width() <= 768) {
+      $(".navbar").hide(); // Replace with your class name
+      $(".navbar, .navbar a").click(function () {
+        $(".navbar").hide();
+      });
+    } else {
+      $(".navbar").show(); // Replace with your class name
+    }
+  }
+
+  // Listen for resize events
+  $(window).resize(hideElementsOnWidth);
+
+  // Initial check
+  $(document).ready(hideElementsOnWidth);
+
+  const truck = $("#truck");
+  setInterval(function () {
+    truck.toggleClass("start");
+    truck.toggleClass("end");
+  }, 5000);
+
+  //Animation In Removals Section
+
+  document
+    .querySelectorAll(".about .image-con .controls .control-btn")
+    .forEach((btn) => {
+      btn.onclick = () => {
+        let src = btn.getAttribute("data-src");
+        document.querySelector(".about .image-con .image").src = src;
+      };
+    });
+});
